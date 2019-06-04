701a4df956d97859de62787961c64b57ccd79655
giovanniguadagnini <giovanni.guadagnini@gmail.com>
2019-06-04 13:13:51 +0200

    added dependencies

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ projects.init(app);
 dataCollections.init(app);
 
 //Launch the server on port 3000
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000 , () => {
   const { address, port } = server.address();
   console.log('Listening at http://localhost:' + port);
 });
